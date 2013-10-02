4802f482094c8fb61e8ce8afbbe4202cc73a1719
Andre Rauh <andre.rauh@gmail.com>
2013-10-01 21:09:26 -0400

    Formatting

--- app/controllers/yield.go
+++ app/controllers/yield.go
@@ -1,12 +1,13 @@
 /*
-This library allows you to use yields and layouts similar to the Rails implementation for the current Revel template implementation.
+This library allows you to use yields and layouts similar to the Rails
+implementation for the current Revel template implementation.
 
 In your app.conf, add a line like
-module.yield=github.com/acsellers/yield
+module.yield = github.com/acsellers/yield
 
-Then instead of starting your controllers from *revel.Controller,
-you can import "github.com/acsellers/yield/app/controllers" and then
-use the struct yield.Controller to embed into your controllers.
+Then instead of starting your controllers from *revel.Controller, you can
+import "github.com/acsellers/yield/app/controllers" and then use the struct
+yield.Controller to embed into your controllers.
 
 Note: the module in that import path is named yield not controllers, and
 that is why you embed yield.Controller not controllers.Controller.
